12d425ebbcc526211215be1e89ecfed7668d5c6c
Dev-Nonsense0909688 <blackplasma001@gmail.com>
2026-02-21 17:18:37 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,60 @@
+const WebSocket = require("ws");
+const readline = require("readline");
+
+const wss = new WebSocket.Server({ port: 8765 });
+
+console.log("Running on ws://localhost:8765");
+
+wss.on("connection", (ws) => {
+    console.log("client connected");
+
+    let name = "client";
+    let path = "~";
+
+    ws.once("message", (data) => {
+        [path, name] = data.toString().split("|", 2);
+        promptLoop();
+    });
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    function ask(q) {
+        return new Promise(res => rl.question(q, res));
+    }
+
+    async function promptLoop() {
+        try {
+            while (ws.readyState === WebSocket.OPEN) {
+
+                if (path.endsWith(":\\")) path = path.slice(0, -2);
+
+                let cmd = await ask(`${name}@${path}$ `);
+
+                if (cmd === "clear" || cmd === "cls") {
+                    console.clear();
+                    continue;
+                }
+
+                ws.send(cmd);
+
+                const reply = await new Promise(res => {
+                    ws.once("message", msg => res(msg.toString()));
+                });
+
+                if (reply.includes("|")) {
+                    [path, name] = reply.split("|", 2);
+                    continue;
+                }
+
+                console.log(reply.trim());
+            }
+        } catch {
+            console.log("\nconnection closed");
+        }
+    }
+
+    ws.on("close", () => console.log("\nclient left"));
+});
